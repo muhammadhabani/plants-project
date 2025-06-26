ed7728f9c27455d18980f2e2c44c4c83f6e4da22
muhammadhabani <muhammadmahguob@gmail.com>
2025-06-26 10:25:17 +0300

    Add files via upload

---
+++ my plants project/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import InteractiveMap from "./components/InteractiveMap";
+
+function App() {
+  return (
+    <div>
+      {/* ... باقي عناصر الموقع ... */}
+      <InteractiveMap />
+    </div>
+  );
+}
+
+export default App;
